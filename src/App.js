880009a793b1f9f00b802e20dfabee8abf93b9da
=Ethan Harvey <sgwethan@outlook.com>
2022-02-16 16:43:44 +0000

    added basic app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import Calendar from "./components/calendar";
+
+import logo from "./lily.png";
+import "./App.css";
 
 function App() {
+  const [isEntered, setIsEntered] = useState(false);
+  const [text, setText] = useState("Creating days..");
+  const texts = ["Creating days..", "Inputting shifts..", "Quitting job.."];
+
+  const openCalendarview = () => {
+    setIsEntered(true);
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setText((prevText) => {
+        var index = texts.indexOf(prevText);
+        return (index + 1 == texts.length) ? texts[0] : texts[index + 1] ;
+      });
+    }, 2000);
+    // clearing interval
+    return () => clearInterval(timer);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> welcome to Lily! </h1>
-      </header>
+      {!isEntered ? (
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>{text}</h2>
+          <button onClick={openCalendarview}> Enter </button>
+        </div>
+      ) : (
+        <Calendar></Calendar>
+      )}
     </div>
   );
 }
