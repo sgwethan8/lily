c99aa58e4f91f317905d1b15e0973a6374623385
=Ethan Harvey <sgwethan@outlook.com>
2022-02-17 19:21:26 +0000

    removed vunlerabilities

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
+const fs = require('fs');
+const shifts = require('..data/shifts.json');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Lily API' });
+  res.render('index', { title: shifts });
 });
 
 module.exports = router;
